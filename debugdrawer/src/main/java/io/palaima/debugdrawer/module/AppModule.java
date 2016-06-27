8d999128855a3db567965f9eab257987f1b1aab3
Justas Medeisis <justas@trafi.com>
2016-06-27 23:10:33 +0300

    Add spinner to select map style to app module.

--- debugdrawer/src/main/java/io/palaima/debugdrawer/module/AppModule.java
+++ debugdrawer/src/main/java/io/palaima/debugdrawer/module/AppModule.java
@@ -22,46 +22,82 @@ public class AppModule implements DrawerModule {
     private final Context context;
     private final String[] endpoints;
     private String selectedEndpoint;
+    private final String[] mapStyles;
+    private String selectedMapStyle;
     private AppModuleListener listener;
 
-    public AppModule(Activity activity, String[] endpoints, String selectedEndpoint) {
+    public AppModule(Activity activity, String[] endpoints, String selectedEndpoint,
+                     String[] mapStyles, String selectedMapStyle) {
         this.context = activity;
+        this.listener = (AppModuleListener) activity;
         this.endpoints = endpoints;
         this.selectedEndpoint = selectedEndpoint;
-        this.listener = (AppModuleListener) activity;
+        this.mapStyles = mapStyles;
+        this.selectedMapStyle = selectedMapStyle;
     }
 
     @NonNull
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup parent) {
-        View view = inflater.inflate(R.layout.debug_drawer_item_app, parent, false);
+        View view = inflater.inflate(R.layout.debug_drawer_module_app, parent, false);
+
+        Spinner endpointSpinner = (Spinner) view.findViewById(R.id.debug_network_endpoint);
+        ArrayAdapter<String> endpointAdapter = new ArrayAdapter<>(context,
+                R.layout.debug_view_spinner_item, endpoints);
+        endpointAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        endpointSpinner.setAdapter(endpointAdapter);
+        for (int i = 0; i < endpoints.length; i++) {
+            if (endpoints[i].equals(selectedEndpoint)) {
+                endpointSpinner.setSelection(i);
+                break;
+            }
+        }
+        endpointSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (endpoints[position].equals(selectedEndpoint)) {
+                    return;
+                }
+                selectedEndpoint = endpoints[position];
+                listener.onEndpointChange(selectedEndpoint);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+        Spinner mapStyleSpinner = (Spinner) view.findViewById(R.id.map_style_endpoint);
+        ArrayAdapter<String> mapStyleAdapter = new ArrayAdapter<>(context,
+                R.layout.debug_view_spinner_item, mapStyles);
+        mapStyleAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mapStyleSpinner.setAdapter(mapStyleAdapter);
+        for (int i = 0; i < mapStyles.length; i++) {
+            if (mapStyles[i].equals(selectedMapStyle)) {
+                mapStyleSpinner.setSelection(i);
+                break;
+            }
+        }
+        mapStyleSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (mapStyles[position].equals(selectedMapStyle)) {
+                    return;
+                }
+                selectedMapStyle = mapStyles[position];
+                listener.onMapStyleChange(selectedMapStyle);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
         Switch logSwitch = (Switch) view.findViewById(R.id.enable_localytics_toasts);
         logSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 listener.onLocalyticsLogSwitchCheckedChanged(isChecked);
-            }
-        });
-        Spinner spinner = (Spinner) view.findViewById(R.id.debug_network_endpoint);
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(context, R.layout.debug_view_spinner_item, endpoints);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
-        for (int i = 0; i < endpoints.length; i++) {
-            if (endpoints[i].equals(selectedEndpoint)) {
-                spinner.setSelection(i);
-                break;
-            }
-        }
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                selectedEndpoint = endpoints[position];
-                listener.onEndpointChange(endpoints[position]);
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
             }
         });
 
@@ -71,6 +107,7 @@ public class AppModule implements DrawerModule {
                 listener.onOnboardingButtonClick();
             }
         });
+
         return view;
     }
 
@@ -95,7 +132,9 @@ public class AppModule implements DrawerModule {
     }
 
     public interface AppModuleListener {
-        void onEndpointChange(String urlText);
+        void onEndpointChange(String url);
+
+        void onMapStyleChange(String url);
 
         void onOnboardingButtonClick();
 
